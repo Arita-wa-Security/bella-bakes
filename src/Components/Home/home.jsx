9564b4126b54255752b69310d4b7cbe6510b3c9e
Samuel <aritasam98@gmail.com>
2023-10-02 19:59:13 +0300

    feat: Text animation

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,13 +1,40 @@
 // import react from 'react'
 import "../Home/home.css"
+import Cake from "../assets/vegan-bread.png"
 
-export default function Home(){
+function Home() {
 
     return (
       <>
         <div className="container-fluid" id="home">
-            ...
+          <div id="paragraph">
+            <h4>Welcome to Bella Bakes</h4>
+            <h3>
+              Your home of <br></br>
+              <span className="wavify">
+                <h1>
+                  <span>V</span>
+                  <span>e</span>
+                  <span>g</span>
+                  <span>a</span>
+                  <span>n</span>
+                  <span> </span>
+                  <span>B</span>
+                  <span>a</span>
+                  <span>k</span>
+                  <span>i</span>
+                  <span>n</span>
+                  <span>g</span>
+                </h1>
+              </span>
+            </h3>
+          </div>
+          <div id="veg-cake">
+            <img src={Cake} alt="bunnies" />
+          </div>
         </div>
       </>
     );
 }
+
+export default Home
