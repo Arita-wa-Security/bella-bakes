f8cdab8e9e2d53c91909cb1e52e4cb5db674391d
Samuel <aritasam98@gmail.com>
2023-11-21 21:12:28 +0300

    feat: Added footer to the about page

---
+++ src/Components/About/about.jsx
@@ -0,0 +1,13 @@
+import "../About/about.css"
+import Footer from "../Home/footer";
+function About(){
+    return (
+      <>
+        <div class="container-fluid">...
+        </div>
+        <Footer />
+      </>
+    );
+}
+
+export default About
